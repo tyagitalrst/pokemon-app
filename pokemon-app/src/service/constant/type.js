64247c492549c1e9a16cfe1a7a6fb788a8d85420
tyagitalrst <tyagitalarasati@gmail.com>
2021-09-14 01:17:03 +0700

    WIP(Pokemon): add new contanst type

---
+++ pokemon-app/src/service/constant/type.js
@@ -0,0 +1,22 @@
+const TYPE_COLOR = {
+  normal: "#c2c2a1",
+  fire: "#fb6c6c",
+  water: "#609fb5",
+  poison: "#7c538c",
+  ground: "#b1736c",
+  bug: "#c3ce75",
+  fighting: "#d6b591",
+  psychic: "#9b7fa6",
+  grass: "#48d0b0",
+  rock: "#a6aab6",
+  electric: "#ffd86f",
+  steel: "#ccccde",
+  ice: "#7fccec",
+  flying: "#bab0d5",
+  ghost: "#735797",
+  dragon: "#f9be00",
+  dark: "#333333",
+  fairy: "#f469a9",
+};
+
+export default TYPE_COLOR;
