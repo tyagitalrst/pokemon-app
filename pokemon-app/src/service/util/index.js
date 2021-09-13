03a5ecbce524d2e248715a87cd070b808fb64e90
tyagitalrst <tyagitalarasati@gmail.com>
2021-09-14 01:16:27 +0700

    WIP(Pokemon): add new util function

--- pokemon-app/src/service/util/index.js
+++ pokemon-app/src/service/util/index.js
@@ -10,3 +10,37 @@ export function generateQueryPagination(totalPage, pageSize) {
     };
   });
 }
+
+export function checkOwnedPokemonName(
+  name,
+  pokemonName,
+  isCollection,
+  pokemonCollections
+) {
+  const ownedPokemon = pokemonCollections.filter(
+    (pokemon) => pokemon.name.toLowerCase() === name.toLowerCase()
+  );
+
+  if (isCollection) {
+    return ownedPokemon.filter(
+      (pokemon) =>
+        pokemon.pokemonName.toLowerCase() === pokemonName.toLowerCase()
+    );
+  } else {
+    return ownedPokemon;
+  }
+}
+
+export function startEndNumberList(current, pageSize) {
+  const start = current * pageSize - (pageSize - 1);
+  const end = current * pageSize;
+  return { start, end };
+}
+
+export function letterCapitalize(str) {
+  return str
+    .split(" ")
+    .map((item) => item.substring(0, 1).toUpperCase() + item.substring(1))
+    .join(" ");
+}
+
